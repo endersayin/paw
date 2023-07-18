57500fd4476cd333d5b15a654dcc04e68d9c28e8
endersayin <ender1444@gmail.com>
2023-07-18 13:31:33 +0300

    paw-06

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,64 @@
-import { IonButton } from '@ionic/react'
-import { useEffect, useState } from 'react'
+import { IonAlert, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonChip, IonContent, IonItem, IonPage } from '@ionic/react'
+import React from 'react'
 
 export const Home = () => {
-	const [data, setData] = useState(0)
-
-	useEffect(() => {
-		setData(100)
-	}, [])
-
 	return (
-		<div>
-			<div>
-				Sayaç : {data} <IonButton onClick={() => setData(data + 1)}>TIKLA</IonButton>
-			</div>
-			<div>Sayaç : {data}</div>
-			<div>Sayaç : {data}</div>
-			<div>Sayaç : {data}</div>
-			<div>Sayaç : {data}</div>
-		</div>
+		<IonPage scrollY>
+			<IonContent>
+				<IonCardContent>
+					<IonCard>
+						<div>
+							<img alt="Silhouette of mountains" src="./kedi-01.jpeg" />
+							<IonCardHeader>
+								<IonCardTitle>Kedi</IonCardTitle>
+								<IonCardSubtitle>Kedi 123456</IonCardSubtitle>
+							</IonCardHeader>
+							<IonItem>
+								<IonChip color="medium">Mavi</IonChip>
+								<IonChip color="medium">Scottish</IonChip>
+								<IonChip color="medium">Kedi</IonChip>
+							</IonItem>
+
+							<IonCardContent>
+								<p>Yapılan araştırmalar kedilerin köpeklerden çok daha bağımsız olduğunu gösteriyor. Kedilere bir şey yaptırmak zordur; onlar kendi bildiklerini yapar.</p>
+								<div>
+									<IonButton style={{ marginTop: '50px' }} id="present-alert" size="medium" expand="block">
+										Click Me
+									</IonButton>
+									<IonAlert trigger="present-alert" header="Alert" subHeader="Important message" message="This is an alert!" buttons={['OK']}></IonAlert>
+								</div>
+							</IonCardContent>
+						</div>
+					</IonCard>
+				</IonCardContent>
+				<IonCardContent>
+					<IonCard>
+						<div>
+							<img alt="Silhouette of mountains" src="./kedi-01.jpeg" />
+							<IonCardHeader>
+								<IonCardTitle>Kedi</IonCardTitle>
+								<IonCardSubtitle>Kedi 123456</IonCardSubtitle>
+							</IonCardHeader>
+							<IonItem>
+								<IonChip color="medium">Mavi</IonChip>
+								<IonChip color="medium">Scottish</IonChip>
+								<IonChip color="medium">Kedi</IonChip>
+							</IonItem>
+
+							<IonCardContent>
+								<p>Yapılan araştırmalar kedilerin köpeklerden çok daha bağımsız olduğunu gösteriyor. Kedilere bir şey yaptırmak zordur; onlar kendi bildiklerini yapar.</p>
+								<div>
+									<IonButton style={{ marginTop: '50px' }} id="present-alert" size="medium" expand="block">
+										Click Me
+									</IonButton>
+									<IonAlert trigger="present-alert" header="Alert" subHeader="Important message" message="This is an alert!" buttons={['OK']}></IonAlert>
+								</div>
+							</IonCardContent>
+						</div>
+					</IonCard>
+				</IonCardContent>
+			</IonContent>
+		</IonPage>
 	)
 }
-
 export default Home
